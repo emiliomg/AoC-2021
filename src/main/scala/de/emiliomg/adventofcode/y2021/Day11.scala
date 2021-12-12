4bdf62dba0e3172b62cbe329ba83edce44936f90
Emilio Markgraf <emilio.markgraf@gmail.com>
2021-12-12 14:42:11 +0100

    Day11, Star1

---
+++ src/main/scala/de/emiliomg/adventofcode/y2021/Day11.scala
@@ -0,0 +1,100 @@
+package de.emiliomg.adventofcode.y2021
+
+import scala.collection.mutable
+import de.emiliomg.adventofcode.y2021.Day11.Position
+
+object Day11 {
+
+  def star1(data: List[String], maxSteps: Int): Long = {
+    val grid   = Grid.fromData(data)
+    val result = LazyList.iterate(grid)(_.step).take(maxSteps + 1)
+
+    // result.foreach(g => pprint.pprintln(g.cells.gridView))
+
+    result.map(_.justFlashed).sum
+  }
+
+  def star2(data: List[String]): Long = {
+    ???
+  }
+
+  case class Position(x: Int, y: Int)
+  type CELLS = Map[Position, Int]
+  case class FlashStep(cells: CELLS, flashesToProcess: List[Position])
+
+  case class Grid(cells: Map[Position, Int]) {
+
+    def step: Grid = {
+      val cellsIncreased: CELLS   = cells.view.mapValues(_ + 1).toMap
+      val flashes: List[Position] = cellsIncreased.filter(_._2 > 9).map(_._1).toList
+
+      def stepFlashes(fs: FlashStep): FlashStep = {
+        val flashNeighbors: Set[Position] = getNeighbors(fs.flashesToProcess.head)
+        val cellsIncreased: CELLS = fs.cells.map {
+          case (pos, octoVal) if flashNeighbors.contains(pos) => pos -> (octoVal + 1)
+          case (pos, octoVal)                                 => pos -> octoVal
+        }.toMap
+        val newlyFlashedNeighbors: List[Position] = flashNeighbors
+          .map(p => p -> cellsIncreased(p))
+          .filter(_._2 == 10)
+          .map(_._1)
+          .toList
+
+        FlashStep(cellsIncreased, fs.flashesToProcess.tail ++ newlyFlashedNeighbors)
+      }
+
+      val cellsAfterFlashes: CELLS = LazyList
+        .iterate(FlashStep(cellsIncreased, flashes))(stepFlashes)
+        .find(_.flashesToProcess.isEmpty)
+        .toList
+        .last
+        .cells
+
+      val finalCells = cellsAfterFlashes.mapValues {
+        case i if i > 9 => 0
+        case i          => i
+      }.toMap
+
+      // pprint.pprintln(finalCells.gridView)
+
+      Grid(finalCells)
+    }
+
+    def justFlashed: Int = cells.values.count(_ == 0)
+
+    def getNeighbors(pos: Position): Set[Position] = {
+      ((
+        for {
+          x <- -1 to 1
+          y <- -1 to 1
+        } yield Position(pos.x + x, pos.y + y)
+      ).toSet - pos)
+        .filter(cells.contains)
+    }
+  }
+
+  object Grid {
+    def fromData(data: List[String]): Grid = {
+      val cells = data.zipWithIndex.flatMap { (line, x) =>
+        line.split("").zipWithIndex.map { (s, y) =>
+          Position(x, y) -> s.toInt
+        }
+      }.toMap
+
+      Grid(cells)
+    }
+  }
+
+  implicit class GridViewForMap(cells: Map[Position, Int]) {
+    def gridView: Array[Array[Int]] = {
+      val maxSize = cells.keySet.maxBy(_.x).x
+      val cellArr = mutable.ArrayBuffer.fill(maxSize + 1, maxSize + 1)(0)
+
+      cells.foreach { (pos, i) =>
+        cellArr(pos.x)(pos.y) = i
+      }
+
+      cellArr.map(_.toArray).toArray
+    }
+  }
+}
---
+++ src/test/scala/de/emiliomg/adventofcode/y2021/Day11Test.scala
@@ -0,0 +1,59 @@
+package de.emiliomg.adventofcode.y2021
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import scala.io.Source
+
+class Day11Test extends AnyFlatSpec with Matchers {
+  "Star 1" should "work with the small test input for 2 days" in {
+    Day11.star1(getSmallTestInput, 2) shouldEqual 9
+  }
+
+  it should "work with the large test input for 10 days" in {
+    Day11.star1(getLargeTestInput, 10) shouldEqual 204
+  }
+
+  it should "work with the large test input for 100 days" in {
+    Day11.star1(getLargeTestInput, 100) shouldEqual 1656
+  }
+
+  it should "work with the puzzle input" in {
+    val result = Day11.star1(getPuzzleInput, 100)
+    pprint.pprintln(s"Day11, Star1: $result")
+    result shouldEqual 1655
+  }
+
+  // "Star 2" should "work with the test input" in {
+  //   Day11.star2(getTestInput) shouldEqual 288957
+  // }
+
+  // it should "work with the puzzle input" in {
+  //   val result = Day11.star2(getPuzzleInput)
+  //   pprint.pprintln(s"Day11, Star2: $result")
+  //   // result shouldEqual 3249889609L
+  // }
+
+  private def getSmallTestInput: List[String] =
+    """11111
+      |19991
+      |19191
+      |19991
+      |11111""".stripMargin.split("\n").toList
+
+  private def getLargeTestInput: List[String] =
+    """5483143223
+      |2745854711
+      |5264556173
+      |6141336146
+      |6357385478
+      |4167524645
+      |2176841721
+      |6882881134
+      |4846848554
+      |5283751526""".stripMargin.split("\n").toList
+
+  private def getPuzzleInput: List[String] = {
+    Source.fromResource("day11.txt").getLines().toList
+  }
+}
